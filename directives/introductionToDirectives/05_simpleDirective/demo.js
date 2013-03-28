965a141c8ace1108923f4fbf773fbde65eaccc30
Ingo Rammer <ingo.rammer@thinktecture.com>
2013-03-28 17:04:20 +0100

    Demo changes

---
+++ directives/introductionToDirectives/05_simpleDirective/demo.js
@@ -0,0 +1,8 @@
+/* Embedded Fragment - controller */function UserController($scope) {
+   $scope.currentUser = {
+      firstName: "John",
+      lastName: "Doe",
+      dateOfBirth: new Date(1980,0,1)
+   };
+}
+/* Fragment End - controller */
