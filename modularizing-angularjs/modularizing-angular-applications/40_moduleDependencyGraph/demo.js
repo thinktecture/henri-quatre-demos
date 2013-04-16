f2b500c76a131a5bd30bfcef0d535a03cf8c61ff
Ingo Rammer <ingo.rammer@thinktecture.com>
2013-04-16 17:55:25 +0200

    Automated publish from content management

--- modularizing-angularjs/modularizing-angular-applications/40_moduleDependencyGraph/demo.js
+++
@@ -1,9 +0,0 @@
-angular.module("RichEditor", []);
-
-angular.module("Folder", []);
-
-angular.module("Inbox", ["Folder"]);
-
-angular.module("Compose", ["RichEditor"]);
-
-angular.module("EmailApplication", ["Inbox", "Compose"]);
